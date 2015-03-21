d6f86b9c52dcf739d401ef7e9bc7be2867a8b6be
TodsDeath <todsdeath@gmx-topmail.de>
2015-03-21 08:23:21 +0100

    new package: MKVToolNix

---
+++ spk/mkvtoolnix/src/installer.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Package
+PACKAGE="mkvtoolnix"
+DNAME="MKVToolNix"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:${PATH}"
+
+preinst ()
+{
+	exit 0
+}
+
+postinst ()
+{
+	# Link
+	ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+	exit 0
+}
+
+preuninst ()
+{
+	exit 0
+}
+
+postuninst ()
+{
+	# Remove link
+	rm -f ${INSTALL_DIR}
+
+	exit 0
+}
+
+preupgrade ()
+{
+	exit 0
+}
+
+postupgrade ()
+{
+	exit 0
+}
